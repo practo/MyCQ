fec6d645558464b374015b06e7974aaf7f333118
Abhinav Lal <abhinav.lall@gmail.com>
2012-07-29 21:29:21 +0530

    Added first 2 questions

--- scripts/populate_questions.py
+++ scripts/populate_questions.py
@@ -3,20 +3,20 @@ from mycq import user_store
 questions = [
     {
         "id": 1,
-        "question": "Sample question 1",
-        "option1": "Sample answer 1",
-        "option2": "Sample answer 2",
-        "option3": "Sample answer 3",
-        "option4": "Sample answer 4",
+        "question": "What percentage of numbers from 1 to 70 have squares that end in the digit 1?",
+        "option1": "1",
+        "option2": "14",
+        "option3": "20",
+        "option4": "21",
         "correct_answer": 3
     },
     {
         "id": 2,
-        "question": "Sample question 2",
-        "option1": "Sample answer 1",
-        "option2": "Sample answer 2",
-        "option3": "Sample answer 3",
-        "option4": "Sample answer 4",
+        "question": "What is the command swapon used for in linux?",
+        "option1": "Create a swap area in RAM",
+        "option2": "Enables swapping of pages",
+        "option3": "Specifies devices on which swapping should take place",
+        "option4": "Creates a swap partition",
         "correct_answer": 3
     },
     {
